5bfa0bb9ee4211751e0898da03a46e7976d9d126
Hamseapdiasiis <hamse906@gmail.com>
2023-05-19 09:42:39 +0300

    exercise test

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("hello");
+let grade = 67;
+
+if (grade => 90 || grade == 100){
+    console.log("You got an A grade");
+}
+
+else if (grade <= 90 || grade >= 75){
+    console.log("You got an B grade");
+}
+
+else if (grade <= 75 || grade >= 60){
+    console.log("You got an C grade");
+}
+
+else {
+    console.log("You failed")
+}
+    
+
