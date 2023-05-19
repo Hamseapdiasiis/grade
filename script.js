aea918505afadc06b8dee3c1ccd04ed277288c52
Hamseapdiasiis <hamse906@gmail.com>
2023-05-19 09:48:04 +0300

    Waa exercise kii oo ka jawaban

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 console.log("hello");
-let grade = 67;
+let grade = 17;
 
-if (grade => 90 || grade == 100){
+if (grade => 90 && grade <= 100){
     console.log("You got an A grade");
 }
 
-else if (grade <= 90 || grade >= 75){
+else if (grade <= 90 && grade >= 75){
     console.log("You got an B grade");
 }
 
-else if (grade <= 75 || grade >= 60){
+else if (grade <= 75 && grade >= 60){
     console.log("You got an C grade");
 }
 
